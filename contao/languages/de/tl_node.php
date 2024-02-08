bbca5cb942b87683e17df69a9c6b35b646a5fdda
MarkejN <markus.nestmann@outlook.com>
2024-02-08 09:29:32 +0100

    Add missing translation

--- contao/languages/de/tl_node.php
+++ contao/languages/de/tl_node.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0+
  */
 
+$GLOBALS['TL_LANG']['tl_node']['cssClass'] = ['Popup CSS-Klasse', 'Hier können Sie eine oder mehrere Klassen eingeben.'];
 $GLOBALS['TL_LANG']['tl_node']['easy_popup_legend'] = 'Easy Popup Einstellungen';
 $GLOBALS['TL_LANG']['tl_node']['easyPopupSettings'] = ['Popup Einstellungen aktivieren', 'Wenn Sie diesen Node als Popup verwenden, können Sie hier die Popup Einstellungen aktivieren.'];
-$GLOBALS['TL_LANG']['tl_node']['cssClass'] = ['Popup CSS-Klasse', 'Hier können Sie eine oder mehrere Klassen eingeben.'];
+$GLOBALS['TL_LANG']['tl_node']['styleManager'] = ['Style Manager'];
--- contao/languages/en/tl_node.php
+++ contao/languages/en/tl_node.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0+
  */
 
+$GLOBALS['TL_LANG']['tl_node']['cssClass'] = ['Popup CSS-Class', 'Here you can enter one or more classes.'];
 $GLOBALS['TL_LANG']['tl_node']['easy_popup_legend'] = 'Easy popup settings';
 $GLOBALS['TL_LANG']['tl_node']['easyPopupSettings'] = ['Enable popup settings', 'If you use this node as a popup, you can enable the popup settings here.'];
-$GLOBALS['TL_LANG']['tl_node']['cssClass'] = ['Popup CSS-Class', 'Here you can enter one or more classes.'];
+$GLOBALS['TL_LANG']['tl_node']['styleManager'] = ['Style Manager'];
