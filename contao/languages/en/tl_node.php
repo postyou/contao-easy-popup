e8e729aa0e471295d6fd11d0a563e896f9c1a5ac
MarkejN <markus.nestmann@outlook.com>
2024-04-29 15:59:01 +0200

    Add more settings

--- contao/languages/en/tl_node.php
+++ contao/languages/en/tl_node.php
@@ -10,7 +10,13 @@ declare(strict_types=1);
  * @license LGPL-3.0+
  */
 
-$GLOBALS['TL_LANG']['tl_node']['cssClass'] = ['Popup CSS-Class', 'Here you can enter one or more classes.'];
+// Legends
 $GLOBALS['TL_LANG']['tl_node']['easy_popup_legend'] = 'Easy popup settings';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_node']['cssClass'] = ['Popup CSS-Class', 'Here you can enter one or more classes.'];
 $GLOBALS['TL_LANG']['tl_node']['easyPopupSettings'] = ['Enable popup settings', 'If you use this node as a popup, you can enable the popup settings here.'];
 $GLOBALS['TL_LANG']['tl_node']['styleManager'] = ['Style Manager'];
+$GLOBALS['TL_LANG']['tl_node']['popupTimeout'] = ['Timeout', 'Here you can enter the time in which the popup should not be displayed again.'];
+$GLOBALS['TL_LANG']['tl_node']['popupDelay'] = ['Delay', 'Here you can enter the time that should pass before the popup is displayed.'];
+$GLOBALS['TL_LANG']['tl_node']['showPopupOnLeave'] = ['Show before leaving the page', 'Show the popup before the user leaves the page.'];
