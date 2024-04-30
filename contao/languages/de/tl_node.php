27e3c856cec8efd734e2adb5c7dbac561f0f3503
MarkejN <markus.nestmann@outlook.com>
2024-04-30 09:41:11 +0200

    Update translations

--- contao/languages/de/tl_node.php
+++ contao/languages/de/tl_node.php
@@ -14,11 +14,11 @@ declare(strict_types=1);
 $GLOBALS['TL_LANG']['tl_node']['easy_popup_legend'] = 'Easy Popup Einstellungen';
 
 // Fields
-$GLOBALS['TL_LANG']['tl_node']['easyPopupSettings'] = ['Popup Einstellungen aktivieren', 'Wenn Sie diesen Node als Popup verwenden, können Sie hier die Popup Einstellungen aktivieren.'];
 $GLOBALS['TL_LANG']['tl_node']['cssClass'] = ['Popup CSS-Klasse', 'Hier können Sie eine oder mehrere Klassen eingeben.'];
-$GLOBALS['TL_LANG']['tl_node']['popupTimeout'] = ['Timeout', 'Hier können Sie die Zeit eingeben, in der das Popup nicht erneut angezeigt werden soll.'];
+$GLOBALS['TL_LANG']['tl_node']['easyPopupSettings'] = ['Popup Einstellungen aktivieren', 'Wenn Sie diesen Node als Popup verwenden, können Sie hier die Popup Einstellungen aktivieren.'];
 $GLOBALS['TL_LANG']['tl_node']['popupDelay'] = ['Verzögerung', 'Hier können Sie die Zeit eingeben, die verstreichen soll, bevor das Popup angezeigt wird.'];
-$GLOBALS['TL_LANG']['tl_node']['showPopupOnLeave'] = ['Verlassen der Seite', 'Hier können Sie den Abstand bis zur oberen Fensterkante eingeben, ab dem das Popup angezeigt werden soll.'];
+$GLOBALS['TL_LANG']['tl_node']['popupTimeout'] = ['Timeout', 'Hier können Sie die Zeit eingeben, in der das Popup nicht erneut angezeigt werden soll.'];
+$GLOBALS['TL_LANG']['tl_node']['showPopupOnLeave'] = ['Vor Verlassen der Seite anzeigen', 'Das Popup wird angezeigt, wenn der Benutzer die Seite verlassen möchte.'];
 $GLOBALS['TL_LANG']['tl_node']['styleManager'] = ['Style Manager'];
 
 // Options
